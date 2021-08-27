828dd7937d69c0763dce687fb19a9944fa4efac0
Fikri Maulidan <78674813+fikrimaulidan99@users.noreply.github.com>
2021-08-28 00:10:26 +0700

    stopped at first and thirty seventh line

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -24,6 +24,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
 
 
 /**
@@ -38,11 +40,15 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+            //addNickname(it)
+        //}
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
